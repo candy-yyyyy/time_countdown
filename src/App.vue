654382d362665195458e3e347e74b495576d0f38
candy-yyyyy <545496535@qq.com>
2022-08-07 21:11:01 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <template>
-  <div></div>
+  <div id="building">
+    <div class="time">
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  created() {
+    const _this = this;
+    console.log(_this);
+    setInterval(function () {
+      let today = new Date();
+      _this.msg =
+        today.getFullYear() +
+        "年" +
+        parseInt(today.getMonth() + 1) +
+        "月" +
+        today.getDate() +
+        "日" +
+        "\xa0" +
+        today.getHours() +
+        "时" +
+        today.getMinutes() +
+        "分" +
+        today.getSeconds() +
+        "秒";
+    }, 1000);
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  margin-top: 0px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+#building {
+  background: url("background.jpeg");
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: 100% 100%;
+}
+
+.time {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 36px;
+  margin-top: 100px;
 }
 </style>
